8942c5c15dfd8c77eeae7db0e70b88c4dddf1b18
Darrel Malisa <darrelmalisa@gmail.com>
2025-07-06 22:07:13 +0200

    First commit

--- Librarymanage/Library_Page.xaml.cs
+++ Librarymanage/Library_Page.xaml.cs
@@ -34,7 +34,7 @@ namespace Librarymanage
         private Frame _mainFrame;
         private string _currentUsername;
         private Book _selectedBook;
-        string connectionString = "Data Source=C:\\Users\\hpie9\\Documents\\Librarymanage\\Librarymanage\\Data\\Library.db;Version=3;";
+        private static string connectionString = "Data Source=Librarymanage\\Data\\Library.db;Version=3;";
         public Library_Page(Frame mainFrame, string username)
         {
             InitializeComponent();
